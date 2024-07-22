3660c03e734c73acf9319401712be3342e01b497
fitrahidayaat <fitrahidayaat@gmail.com>
2024-07-22 08:27:34 +0700

    all functionality completed'

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,34 @@ function App() {
     const [projectsState, setProjectsState] = useState({
         selectedProjectId: undefined,
         projects: [],
+        tasks: [],
     });
+
+    function handleAddTask(text) {
+        setProjectsState((prevState) => {
+            const taskId = Math.random();
+            const newTask = {
+                text: text,
+                projectId: prevState.selectedProjectId,
+                id: taskId,
+            };
+            return {
+                ...prevState,
+                tasks: [newTask, ...prevState.tasks],
+            };
+        });
+    }
+
+    function handleDeleteTask(id) {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                tasks: prevState.tasks.filter(
+                    (task) => task.id !== id
+                ),
+            };
+        });
+    }
 
     function handleSelectProject(id) {
         setProjectsState((prevState) => {
@@ -57,17 +84,34 @@ function App() {
             return {
                 ...prevState,
                 selectedProjectId: undefined,
-                projects: prevState.projects.filter(project => project.id !== prevState.selectedProjectId),
+                projects: prevState.projects.filter(
+                    (project) => project.id !== prevState.selectedProjectId
+                ),
             };
         });
     }
 
-    const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+    const selectedProject = projectsState.projects.find(
+        (project) => project.id === projectsState.selectedProjectId
+    );
 
-    let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
+    let content = (
+        <SelectedProject
+            project={selectedProject}
+            onDelete={handleDeleteProject}
+            onAddTask={handleAddTask}
+            onDeleteTask={handleDeleteTask}
+            tasks={projectsState.tasks}
+        />
+    );
 
     if (projectsState.selectedProjectId === null) {
-        content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>;
+        content = (
+            <NewProject
+                onAdd={handleAddProject}
+                onCancel={handleCancelAddProject}
+            />
+        );
     } else if (projectsState.selectedProjectId === undefined) {
         content = (
             <NoProjectSelected onStartAddProject={handleStartAddProject} />
@@ -80,6 +124,7 @@ function App() {
                 onSelectProject={handleSelectProject}
                 onStartAddProject={handleStartAddProject}
                 projects={projectsState.projects}
+                selectedProjectId={projectsState.selectedProjectId}
             />
             {content}
         </main>
--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,6 @@
-export default function SelectedProject({ project, onDelete }) {
+import Tasks from "./Tasks";
+
+export default function SelectedProject({ project, onDelete, onAddTask, onDeleteTask, tasks }) {
     const formattedDate = new Date(project.dueDate).toLocaleDateString(
         "en-US",
         {
@@ -28,6 +30,7 @@ export default function SelectedProject({ project, onDelete }) {
                     {project.description}
                 </p>
             </header>
+            <Tasks onAdd={onAddTask} onDelete={onDeleteTask} tasks={tasks} />
         </div>
     );
 }
